34c08780f2f29901b0808d203c8de014b7311161
Michael Sledge <michael.sledge@incrowdsports.com>
2020-07-07 17:41:07 +0100

    cache jikan requests locally

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 import re
+import time
 from mycroft import MycroftSkill, intent_file_handler, intent_handler
 from jikanpy import Jikan
 
@@ -10,9 +11,9 @@ class Mal(MycroftSkill):
         MycroftSkill.__init__(self)
 
     def initialize(self):
-        self.jikan = Jikan()
-        self.alias_helper = AnimeAliasHelper()
         self.mal_username = self.settings.get(USERNAME_SETTING_KEY)
+        self.jikan = CachedJikan(self.mal_username)
+        self.alias_helper = AnimeAliasHelper(self.jikan)
 
     def on_settings_changed(self):
         self.mal_username = self.settings.get(USERNAME_SETTING_KEY, False)
@@ -28,7 +29,7 @@ class Mal(MycroftSkill):
         requested_anime = message.data.get('show')
 
         if requested_anime is not None:
-            animelist_watching = self.jikan.user(username=self.mal_username, request='animelist', argument='watching')
+            animelist_watching = self.jikan.animelist_watching()
             
             for anime in animelist_watching['anime']:
                 self.log.debug(str.format("checking show id {}. title: '{}'", anime['mal_id'], anime['title']))
@@ -66,11 +67,11 @@ def create_skill():
 
 
 class AnimeAliasHelper():
-    def __init__(self):
-        self.jikan = Jikan()
+    def __init__(self, cached_jikan):
+        self.jikan = cached_jikan
     
     def name_matches_anime(self, name, anime_id):
-        anime = self.jikan.anime(anime_id) # could cache this locally?
+        anime = self.jikan.anime(anime_id)
         titles = [anime['title'], anime['title_english'], anime['title_japanese']]
         titles.extend(anime['title_synonyms'])
         # strip punctuation and whitespace
@@ -78,3 +79,45 @@ class AnimeAliasHelper():
         name_norm = re.sub(r'([^\w]|_)', '', name.lower())
         
         return any(title.startswith(name_norm) for title in titles_norm)
+
+class CachedJikan():
+    def __init__(self, username):
+        self._jikan = Jikan()
+        self._last_request_time = time.time()
+        self._REQUEST_DELAY = 2
+        self._mal_username = username
+        self._animelist_watching = None
+        self._animelist_watching_time = None
+        self._animes = dict()
+
+    def _request_wait_time(self):
+        return max(0, (self._last_request_time +self._REQUEST_DELAY) - time.time())
+
+    
+    def _delay_request(self, request_func):
+        time.sleep(self._request_wait_time())
+        result = request_func()
+        self._last_request_time = time.time()
+        return result
+
+
+    
+    def animelist_watching(self):
+        if self._animelist_watching is None or time.time() - self._animelist_watching_time > 5 * 60:
+            def do_request():
+                return self._jikan.user(username=self._mal_username, request='animelist', argument='watching')
+            self._animelist_watching = self._delay_request(do_request)
+        return self._animelist_watching
+
+
+    def anime(self, anime_id):
+        if anime_id not in self._animes:
+            try:
+                def do_request():
+                    return self._jikan.anime(anime_id)
+                anime = self._delay_request(do_request)
+                self._animes[anime_id] = anime
+            except:
+                # todo: log line here
+                return None
+        return self._animes[anime_id]
