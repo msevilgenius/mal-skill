35c8e098e1a77ab43dce11e92f93196a34984540
Michael Sledge <michael.sledge@incrowdsports.com>
2020-07-07 16:59:32 +0100

    fix using appending lists

--- __init__.py
+++ __init__.py
@@ -72,7 +72,7 @@ class AnimeAliasHelper():
     def name_matches_anime(self, name, anime_id):
         anime = self.jikan.anime(anime_id) # could cache this locally?
         titles = [anime['title'], anime['title_english'], anime['title_japanese']]
-        titles.append(anime['title_synonyms'])
+        titles.extend(anime['title_synonyms'])
         # strip punctuation and whitespace
         titles_norm = [re.sub(r'([^\w]|_)', '', t.lower()) for t in titles]
         name_norm = re.sub(r'([^\w]|_)', '', name.lower())
