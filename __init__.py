59db18b2a3dcea9d63cf652bbef2231d17ee140d
Michael Sledge <git@michael.sledge.me>
2020-07-07 14:37:46 +0100

    Implement basic next episode skill

--- __init__.py
+++ __init__.py
@@ -1,13 +1,66 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_file_handler, intent_handler
+from jikanpy import Jikan
 
 
 class Mal(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
+    def initialize(self):
+        self.jikan = Jikan()
+        self.mal_username = self.settings.get('username')
+
+    def on_settings_changed(self):
+        self.mal_username = self.settings.get('username', False)
+        # self.trigger_time_display(show_time)
+
     @intent_file_handler('mal.intent')
     def handle_mal(self, message):
         self.speak_dialog('mal')
+
+    #@intent_handler(IntentBuilder('ThankYouIntent').require('ThankYouKeyword'))
+    #@intent_handler('HowAreYou.intent')
+    @intent_handler('next.episode.intent')
+    def handle_next_episode_intent(self, message):
+        self.log.info('next episode intent')
+
+        if not self.mal_username:
+            self.log.info('mal username not set')
+            self.speak_dialog('I don\'t know your mal username')
+            return
+
+        show = message.data.get('show')
+        if show is not None:
+            animelist_watching = self.jikan.user(username=self.mal_username, request='animelist', argument='watching')
+            reply = "I couldn't find any info for {}".format(show)
+            for anime in animelist_watching['anime']:
+                self.log.info("checking show id {}. title: '{}'".format(anime['mal_id'], anime['title']))
+                if anime['title'].lower().startswith(show):
+                   reply =  "The next episode is {} of {}".format(anime['watched_episodes'] + 1, anime['total_episodes'])
+            self.speak_dialog(reply)
+        else:
+            self.speak_dialog('I didn\'t catch the name of the show')
+
+
+    # @intent_handler()
+    # def handle_plan_to_watch_intent(self, message):
+    #     animelist_ptw = self.jikan.user(username=self.mal_username, request='animelist', argument='ptw')
+    #     self.log.info('plan to watch intent')
+    #     self.speak_dialog(message)
+
+    # def converse(self, utterances, lang):
+    #     if utterances and self.voc_match(utterances[0], 'understood'):
+    #         self.speak_dialog('great')
+    #         return True
+    #     else:
+    #         return False
+
+    def stop(self):
+        pass
+    
+    # def shutdown(self):
+    #     self.cancel_scheduled_event('my_event')
+    #     self.stop_my_subprocess()
 
 
 def create_skill():
